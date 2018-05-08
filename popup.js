47a92e668c2819a07a83f966521918d917a275e9
Yael <Yael.bs1@gmail.com>
2018-05-08 19:05:37 +0300

    Add events handlers to '#calculator span's

--- popup.js
+++ popup.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() 
 {
+	var keys = document.querySelectorAll('#calculator span'); // Get all the keys from document
+	var operators = ['+', '-', '*', '/'];
+	var decimalAdded = false;
+
+	// Add onclick event to all the keys and perform operations
+	for(var i = 0; i < keys.length; i++) 
+	{
+		keys[i].onclick = function(e) 
+		{
+			
+			// prevent page jumps
+			e.preventDefault();
+		} 
+	}
 
 }, false); // 'DOMContentLoaded'
